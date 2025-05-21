57d30dfc76a1079267cef5c97192a09930e208bf
rhygon <rhygonfn@gmail.com>
2025-05-21 00:21:17 -0400

    welp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,15 +12,15 @@ function mod(n: number, m: number) {
 function setCookie(name: string,value: string,seconds:number) {
     let expires = "";
     if (seconds) {
-        let date = new Date();
+        const date = new Date();
         date.setTime(date.getTime() + (seconds*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
     }
     document.cookie = name + "=" + (value || "")  + expires + "; path=/";
 }
 function getCookie(name: string) {
-    let nameEQ = name + "=";
-    let ca = document.cookie.split(';');
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
     for(let i=0;i < ca.length;i++) {
         let c = ca[i];
         while (c.charAt(0)==' ') c = c.substring(1,c.length);
